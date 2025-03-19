759fc9a58a772ffc81a3fc1c4454edfada6a7b56
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2025-03-19 22:27:11 +0200

    fetching stop id's

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template, request
-import time, requests
+import time, requests, re
 
 app = Flask(__name__)
 
-API_osoite = "https://api.digitransit.fi/routing/v2/hsl/gtfs/v1" + \
+graphql_addr = "https://api.digitransit.fi/routing/v2/hsl/gtfs/v1" + \
         "?digitransit-subscription-key=754a004ba87c4fdd8506532327e212c0"
 
+search_addr = "https://api.digitransit.fi/geocoding/v1/search"
+
+search_params = {
+        "digitransit-subscription-key": "954b08e64f1f465ba4c03d0d88cf69e2",
+        "lang": "fi",
+        "sources": "gtfsHSL,gtfsHSLlautta",
+        "layers": "stop,station",
+}
+
+def hsl_stop_id(stop_id):
+    osuma = re.search(r"HSL:[0-9]+", stop_id)
+    return osuma and osuma.group(0)
+
+def hae_pysakit(nimi_tai_osa):
+    pysakit = requests.get(search_addr,
+            params={**search_params, "text": nimi_tai_osa}).json()
+    return {hsl_stop_id(feature['properties']['id']): feature['properties']
+            for feature in pysakit['features']}
+
 def hae_pysakkitiedot(pysakki):
-    return requests.post(API_osoite, json=dict(
+    return requests.post(graphql_addr, json=dict(
         query=open("stop-query.gql").read(),
         variables=dict(
             numberOfDepartures=10,
@@ -20,12 +39,19 @@ def hae_pysakkitiedot(pysakki):
 def root():
     return render_template('lomake.html')
 
-@app.route("/vastaus")
-def vastaus():
-    pysakki = request.args.get('pysakki', 'HSL:1282103')
-    pysakkitiedot = hae_pysakkitiedot(pysakki)['data']['stop']
+def aikataulusivu(pysakkitiedot):
     return render_template('vastaus.html',
             url=pysakkitiedot['url'],
             pysahdykset=pysakkitiedot['stoptimes'],
             pysakki=pysakkitiedot['name'])
+
+def pysakkihakusivu(pysakki):
+    return "Nothing here yet"
+
+@app.route("/vastaus")
+def vastaus():
+    pysakki = request.args.get('pysakki', 'HSL:1282103')
+    pysakkitiedot = hae_pysakkitiedot(pysakki)['data']['stop']
+    if pysakkitiedot: return aikataulusivu(pysakkitiedot)
+    else: return pysakkihakusivu(pysakki)
 
